612ebd63bca89c05191307854b6044ab092d03ce
Abhishek Bisht <65127203+Commaando@users.noreply.github.com>
2025-04-10 00:39:42 +0530

    FEAT: husky , eslint and prettier addition.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,11 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: ['@react-native', 'plugin:react/recommended', 'prettier'],
+  plugins: ['react', 'react-native', 'import'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'import/order': ['warn', { 'newlines-between': 'always' }],
+    'max-len': ['warn', { code: 100 }],
+    
+  },
 };
--- src/screens/homescreen/home-screen.tsx
+++ src/screens/homescreen/home-screen.tsx
@@ -1,48 +1,50 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { View, Text, Button, ActivityIndicator, StyleSheet } from 'react-native';
-import { fetchDummyData } from '../../services/api'; // Assuming you have this service
+import { View, Text, Button, ActivityIndicator } from 'react-native';
+
+import { fetchDummyData } from '../../services/api';
+
 import { styles } from './home-screen.styles';
 
 const useFetchData = () => {
-    const initialCallRef = useRef(false)
-    const [data, setData] = useState<string | null>(null);
-    const [loading, setLoading] = useState<boolean>(false);
-    const [error, setError] = useState<string | null>(null);
+  const initialCallRef = useRef(false);
+  const [data, setData] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
 
-    const handleApiCall = async () => {
-        setLoading(true);
-        setError(null);
-        try {
-            const response = await fetchDummyData();
-            initialCallRef.current = true
-            console.log(response)
-            setData(response.message as string)
-        } catch (err) {
-            setError('Failed to fetch data');
-        } finally {
-            setLoading(false);
-        }
-    };
-
-    const retry = () => {
-        handleApiCall();
+  const handleApiCall = async () => {
+    setLoading(true);
+    setError(null);
+    try {
+      const response = await fetchDummyData();
+      initialCallRef.current = true;
+      console.log(response);
+      setData(response.message as string);
+    } catch (err) {
+      setError('Failed to fetch data');
+    } finally {
+      setLoading(false);
     }
+  };
 
-    useEffect(() => {
-        handleApiCall();
-    }, [])
+  const retry = () => {
+    handleApiCall();
+  };
 
-    return {data, loading, error, retry}
-}
+  useEffect(() => {
+    handleApiCall();
+  }, []);
+
+  return { data, loading, error, retry };
+};
 
 export const HomeScreen: React.FC = () => {
-    const {data, error, loading, retry } = useFetchData()
-    return (
-        <View style={styles.container}>
-            {loading && <ActivityIndicator size="large" color="#0000ff" />}
-            {error && <Text style={styles.errorText}>{error}</Text>}
-            {data && <Text style={styles.dataText}>{data}</Text>}
-            <Button title="Retry" onPress={retry} />
-        </View>
-    );
+  const { data, error, loading, retry } = useFetchData();
+  return (
+    <View style={styles.container}>
+      {loading && <ActivityIndicator size='large' color='#0000ff' />}
+      {error && <Text style={styles.errorText}>{error}</Text>}
+      {data && <Text style={styles.dataText}>{data}</Text>}
+      <Button title='Retry' onPress={retry} />
+    </View>
+  );
 };
